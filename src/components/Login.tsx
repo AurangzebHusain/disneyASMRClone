1c4a3e6f1faa13576e1be400cc6041d99763f19e
AurangzebHusain <aurangzeb.husain@adroit-vantage.com>
2022-08-20 20:02:16 +0530

    Added button and text

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -30,13 +30,41 @@ function Login() {
         <BgImage />
         <CTA>
           <CTALogoOne src="/public/images/cta-logo-one.svg" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
           <CTALogoTwo src="/public/images/cta-logo-two.png" />
         </CTA>
       </Content>
     </Container>
   );
 }
-
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 0.9rem;
+  line-height: 1.5;
+  letter-spacing: 5px;
+  margin: 0 1rem;
+`;
+const SignUp = styled.a`
+  outline: none;
+  font-weight: bold;
+  border: none;
+  width: 100%;
+  padding: 1rem;
+  text-align: center;
+  letter-spacing: 1.5px;
+  font-size: 1.1rem;
+  background-color: #0063e5;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
 const BgImage = styled.div`
   background-image: url("/images/login-background.jpg");
   height: 100%;
@@ -60,9 +88,13 @@ const CTA = styled.div`
   transition: opacity 2s;
 `;
 const CTALogoOne = styled.img`
+  cursor: pointer;
   width: 100%;
+  max-width: 600px;
 `;
 const CTALogoTwo = styled.img`
   width: 100%;
+  cursor: pointer;
+  max-width: 600px;
 `;
 export default Login;
